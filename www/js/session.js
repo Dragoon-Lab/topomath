d3f262c8d05a5e9ec141f716da4167cc40051131
Ritesh Reddy Samala <RiteshSamala@riteshs-mbp.mobile.asu.edu>
2019-04-23 15:43:29 -0700

    review changes and getStartID logic updated to getLargestID

--- www/js/session.js
+++ www/js/session.js
@@ -54,17 +54,11 @@ define([
 				handleAs: "json"
 			}).then(lang.hitch(this, function(model_object){
 				console.log("model object received", model_object);
-				if(params.sp == "on" || params.ss == "on"){
+				if(params.gp == "on" || params.gs == "on"){
 					//check for student 
 					var TM = new tweakModel(model_object);
-					var new_obj = TM.updateModel(params.sp, params.ss);
+					var new_obj = TM.updateModel(params.gp, params.gs);
 				}
-				/*
-				if(params.ss == "on"){
-					console.log("show drts(general schema) switch on");
-					var TM = new tweakModel(model_object);
-					var new_obj = TM.showSchemas("DRT");
-				}*/
 				return model_object;
 			}), lang.hitch(this, function(err){
 				console.log("no model found", err);
--- www/js/tweakModel.js
+++ www/js/tweakModel.js
@@ -1,7 +1,8 @@
 define([
 	"dojo/_base/declare", 
 	"dojo/_base/array",
-	"./equation"
+	"./equation",
+	"./model"
 ], function(declare, array, equation){
 	return declare(null,{
 
@@ -10,57 +11,11 @@ define([
 			this.authObj = model_object.authorModelNodes;
 			this.studObj = model_object.studentModelNodes;
 		},
-		/*
-		copyParams: function(){
-			var getNewID = this.getStartID()+1;
-			var snodesCount = 0;
-			var rightStatus = {
-				description: {status: "correct", disabled: true},
-				qtyDescription: {status: "correct"},
-				units: {status: "correct", disabled: true},
-				value: {status: "correct", disabled: true},
-				variable: {status: "correct", disabled: true},
-				variableType: {status: "correct", disabled: true}
-			}
-			//traverse through authObj
-			for(var prop in this.authObj){
-				//console.log("current prop is", prop, this.authObj.hasOwnProperty(prop));
-				if(this.authObj.hasOwnProperty(prop) && this.authObj[prop].hasOwnProperty("type") && this.authObj[prop].type == "quantity"){
-					var curAuthID = this.authObj[prop].ID;
-					var parentDet = this.findParentDetails(curAuthID);
-					this.curObj["studentModelNodes"][snodesCount++] = {
-						ID: "id" + getNewID++,
-						attemptCount: {
-							schema: 1,
-							entity: 0,
-							variables: 0,
-						},
-						authoredID: curAuthID,
-						color: this.authObj[prop].color,
-						description: this.authObj[prop].description,
-						links: this.authObj[prop].links,
-						parentSchema: parentDet.schema,
-						parentEquation: parentDet.equation,
-						position: this.authObj[prop].position,
-						selfEquation: this.authObj[prop].selfEquation,
-						status: rightStatus,
-						type: "quantity",
-						units: this.authObj[prop].units,
-						value: this.authObj[prop].value,
-						variable: this.authObj[prop].variable,
-						variableType: this.authObj[prop].variableType
-					}
-					this.updateAssistanceScore(curAuthID); 
-				}
-			}
-		},
-		*/
 
-		updateModel: function(showParams, showSchema){
-			console.log("shows", showParams, showSchema);
+		updateModel: function(giveParams, giveSchema){
+			console.log("giving", giveParams, giveSchema);
 			var schema = "DRT";
-			var getNewID = this.getStartID()+1;
-			var newObj = {};
+			var getNewID = this.getLargestID()+1;
 			var snodesCount = 0;
 			var rightQtyStatus = {
 				description: {status: "correct", disabled: true},
@@ -77,18 +32,19 @@ define([
 				schemas: {status: "correct"},
 			};
 			var authStudIDMap = [];
-			//traverse through authObj
+			//Initially fill in authStudIDMap with existing student node's authorId=studentID. Using this later what nodes have to be added or ignored can be decided
 			for(var prop in this.studObj){
 				if(this.studObj.hasOwnProperty(prop) && this.studObj[prop].hasOwnProperty("ID")){
 					authStudIDMap[this.studObj[prop].authoredID] = this.studObj[prop].ID;
 				}
 			}
+			//traverse through authObj
 			for(var prop in this.authObj){
 				//console.log("current prop is", prop, this.authObj.hasOwnProperty(prop));
 				if(this.authObj.hasOwnProperty(prop) && this.authObj[prop].hasOwnProperty("type") && this.authObj[prop].type == "quantity"){
 					var curAuthID = this.authObj[prop].ID;
 					var parentDet = this.findParentDetails(curAuthID);
-					var schemaCheck = (showSchema == "on" && showParams != "on") ? ( parentDet.schema == schema ) : (showParams == "on"? true: false);
+					var schemaCheck = (giveSchema == "on" && giveParams != "on") ? ( parentDet.schema == schema ) : (giveParams == "on"? true: false);
 					if(schemaCheck && !authStudIDMap[curAuthID]){
 							this.studObj[snodesCount++] = {
 							ID: "id" + getNewID,
@@ -118,7 +74,7 @@ define([
 					} 
 				}
 			}
-			if(showSchema == "on"){
+			if(giveSchema == "on"){
 				for(var prop in this.authObj){
 				if(this.authObj.hasOwnProperty(prop) && this.authObj[prop].hasOwnProperty("type") && this.authObj[prop].type == "equation"){
 					var curAuthID = this.authObj[prop].ID;
@@ -150,12 +106,33 @@ define([
 				}
 				}	
 			}
-			console.log("updated DRTs obj", newObj);
 		},
-		getStartID: function(){
-			var authObjLen = Object.keys(this.authObj).length;
-			var studObjLen = Object.keys(this.studObj).length;
-			return authObjLen+studObjLen;
+		getLargestID: function(){
+			//this function returns the largest number on the node ids from the current author and student model nodes
+			var largest = 0;
+			//traverse auth nodes
+			for(var prop in this.authObj){
+				if(this.authObj.hasOwnProperty(prop) && this.authObj[prop].hasOwnProperty("ID")){
+					var nodeID = this.authObj[prop].ID;
+					if(nodeID.length >= 2 && nodeID.slice(0, 2) == "id"){
+						var n = parseInt(nodeID.slice(2));
+						if(n && n > largest)
+						largest = n;
+					}
+				}
+			}
+			//traverse stud nodes
+			for(var prop in this.studObj){
+				if(this.studObj.hasOwnProperty(prop) && this.studObj[prop].hasOwnProperty("ID")){
+					var node = this.studObj[prop].ID;
+					if(nodeID.length >= 2 && nodeID.slice(0, 2) == "id"){
+						var n = parseInt(nodeID.slice(2));
+						if(n && n > largest)
+						largest = n;
+					}
+				}
+			}
+			return largest;
 		},
 
 		findParentDetails: function(nodeID){
