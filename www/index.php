6d5891c88d0439cddb5752c4d5d0e8220b4e961c
riteshsamala <rsamala@asu.edu>
2020-03-03 13:55:26 -0700

    radios moved into label to see if touch screen bug is fixed: just a test

--- www/index.php
+++ www/index.php
@@ -319,10 +319,15 @@
 					<div id="variableTypeQuestionMark" class="questionMark"></div>
 					<label>Type</label>
 				</span>
-				<input data-dojo-type="dijit/form/RadioButton" name="variableType" class="handleVariable" id="unknownType" value="unknown"/>
-				<label for="unknownType">Unknown</label>
-				<input data-dojo-type="dijit/form/RadioButton" name="variableType" class="handleVariable" id="parameterType" value="parameter"/>
-				<label for="parameterType">Parameter</label>
+				<span>
+				<label for="unknownType">
+					<input data-dojo-type="dijit/form/RadioButton" name="variableType" class="handleVariable" id="unknownType" value="unknown"/>
+					Unknown
+				</label>
+				<label for="parameterType">
+					<input data-dojo-type="dijit/form/RadioButton" name="variableType" class="handleVariable" id="parameterType" value="parameter"/>
+					Parameter
+				</label>
 				<span id="variableTypeDynamicOption" style="display: none;">
 					<input data-dojo-type="dijit/form/RadioButton" name="variableType" class="handleVariable" id="dynamicType" value="dynamic"/>
 					<label for="dynamicType">Dynamic</label>
